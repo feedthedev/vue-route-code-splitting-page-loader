bcaff0f6f5060428d62acab3dd9ad22698dbd3bb
Alex Seifert <alex@alexseifert.com>
2023-04-25 07:42:12 +0200

    Add the PageLoader component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import PageLoader from './components/PageLoader.vue'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <PageLoader />
+
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
